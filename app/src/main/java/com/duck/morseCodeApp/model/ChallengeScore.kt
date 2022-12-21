bf3ed3e8ada47cc53f75f40e700403a246a10c0d
menisk7 <menisk7@gmail.com>
2022-12-21 23:09:32 +0100

    fixed typo

--- app/src/main/java/com/duck/morseCodeApp/model/ChallengeScore.kt
+++ app/src/main/java/com/duck/morseCodeApp/model/ChallengeScore.kt
@@ -3,6 +3,6 @@ package com.duck.morseCodeApp.model
 data class ChallengeScore(var user: String,
                           val points:Int){
     override fun toString(): String {
-        return "$user ~$points"
+        return "$user $points"
     }
 }
