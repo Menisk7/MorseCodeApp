6559210c0c0c43b4f601c4617d3f5b2a5997b653
menisk7 <menisk7@gmail.com>
2022-12-21 21:37:40 +0100

    model

---
+++ app/src/main/java/com/duck/morseCodeApp/model/ChallengeScore.kt
@@ -0,0 +1,8 @@
+package com.duck.morseCodeApp.model
+
+data class ChallengeScore(var user: String,
+                          val points:Int){
+    override fun toString(): String {
+        return "$user ~$points"
+    }
+}
