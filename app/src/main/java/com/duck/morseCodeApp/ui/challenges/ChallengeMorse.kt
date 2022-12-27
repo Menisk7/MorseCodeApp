27100be8c6a5ebdb599829f07a33f00460461c2c
menisk7 <menisk7@gmail.com>
2022-12-27 03:10:25 +0100

    clean up/added logic for the whole generating challenges, adding points and writing down score in viewModel

--- app/src/main/java/com/duck/morseCodeApp/ui/challenges/ChallengeMorse.kt
+++ app/src/main/java/com/duck/morseCodeApp/ui/challenges/ChallengeMorse.kt
@@ -26,23 +26,30 @@ class ChallengeMorse : Fragment() {
     private lateinit var challengeViewModel: ChallengeViewModel
     private var _binding: MorseChallengeBinding? = null
     private val binding get() = _binding!!
-    private var numberOfChallenge:Int=0
+    private var numberOfChallenge:Int=1
+    private var chosenNumberOfChallenges:Int=10
+    private val morseCode=MorseCode()
+    private var points:Int=0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
       _binding = MorseChallengeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         setup()
         val factory= InjectorUtils.provideChallengeViewModelFactory()
          challengeViewModel =
             //this returns challengeViewModel which manipulates challengeRepository
-            ViewModelProvider(this, factory).get(ChallengeViewModel::class.java)
-        val cs:ChallengeScore=ChallengeScore("testUser",10)
+             ViewModelProvider(this, factory)[ChallengeViewModel::class.java]
+        val username=requireArguments().getString("userName")!!
+        val cs =ChallengeScore(username,0)
         //only for testing
-        //challengeViewModel.addScore(cs)
-        var test=arguments?.getInt("key")
+        challengeViewModel.addScore(cs)
+        val users=challengeViewModel.getUsers()
+
+        val user = users.find { it == "test" }
+        var useg=user
 
         return root
 
@@ -51,9 +58,9 @@ class ChallengeMorse : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         super.onViewCreated(view, savedInstanceState)
-
+        val action = ChallengeMorseDirections.actionMorseChallengeToScoreScreen()
         binding.enterBtn.setOnClickListener{
-            validateInput()
+            process()
         }
         binding.dashBtn.setOnClickListener {
             binding.inputText.append("-")
@@ -72,33 +79,68 @@ class ChallengeMorse : Fragment() {
             binding.inputText.append("  ")
         }
         binding.next.setOnClickListener {
-            val action = ChallengeMorseDirections.actionMorseChallengeToScoreScreen()
             findNavController().navigate(action)
         }
 
     }
 
     private fun setup(){
-        val morseCode=MorseCode()
+        chosenNumberOfChallenges = requireArguments().getInt("numberOfChallenges")
+        nextChallengeUpdate()
+        updateProgressTextView()
+    }
+
+    private fun updateProgressTextView(){
+        val score="$numberOfChallenge/$chosenNumberOfChallenges"
+        binding.progressTextView.text = score
+    }
+
+    private fun nextChallengeUpdate(){
         binding.promptText.setText(morseCode.generateMorseChallenge())
     }
-    fun validateInput(){
 
-        val morseCode= MorseCode()
-        val encodedLetter=morseCode.encode(binding.promptText.text.toString())
-        val userInput=morseCode.decode(binding.inputText.text.toString())
-        println(userInput + " userInput")
-        println(encodedLetter + "encodedLetter")
-        if(userInput == binding.promptText.text.toString()) {
-            ++numberOfChallenge
-            Toast.makeText(binding.root.context.applicationContext, "Correct!", Toast.LENGTH_LONG)
-                .show()
+    private fun clearEditTexts() {
+        binding.inputText.text.clear()
+        binding.promptText.text.clear()
+    }
 
-
+    private fun toNextChallenge(state:Boolean){
+        if(state){
+            updateProgressTextView()
+            clearEditTexts()
+            nextChallengeUpdate()
         }
-        else
-            Toast.makeText(binding.root.context.applicationContext,"Wrong!",Toast.LENGTH_LONG).show()
+    }
 
+    private fun showResponse(isCorrect:Boolean)
+    {
+        if(isCorrect)
+            Toast.makeText(binding.root.context.applicationContext, "Correct!", Toast.LENGTH_LONG).show()
+        else
+        Toast.makeText(binding.root.context.applicationContext,"Wrong!",Toast.LENGTH_LONG).show()
+    }
+    private fun setFinalScore()
+    {challengeViewModel.modifyScore(requireArguments().getString("userName")!!,points)}
+
+    private fun validateInput(): Boolean {
+        val userInput=morseCode.decode(binding.inputText.text.toString())
+        return userInput == binding.promptText.text.toString()
+    }
+    private fun process(){
+        if (validateInput()) {
+            ++points
+            ++numberOfChallenge
+            showResponse(true)
+            toNextChallenge(numberOfChallenge <= chosenNumberOfChallenges)
+        }else{
+            showResponse(false)
+        }
+        if(numberOfChallenge>chosenNumberOfChallenges)
+        {
+            setFinalScore()
+            val action = ChallengeMorseDirections.actionMorseChallengeToScoreScreen()
+            findNavController().navigate(action)
+        }
     }
 
 override fun onDestroyView() {
