c37e7b6dff8808978592a8ce80a7704afb8f3083
menisk7 <menisk7@gmail.com>
2023-03-13 22:45:49 +0100

    RecyclerView implemented

--- app/src/main/java/com/duck/morseCodeApp/ui/score/ScoreScreen.kt
+++ app/src/main/java/com/duck/morseCodeApp/ui/score/ScoreScreen.kt
@@ -16,11 +16,14 @@ import com.duck.morseCodeApp.ui.challenges.ChallengeViewModel
 import com.duck.morseCodeApp.util.InjectorUtils
 import androidx.activity.OnBackPressedDispatcherOwner
 import androidx.activity.addCallback
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.duck.morseCodeApp.ui.mainscreen.MainActivity
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
+private lateinit var recyclerView: RecyclerView
 class ScoreScreen : Fragment(), OnBackPressedDispatcherOwner {
 
     private lateinit var challengeViewModel: ChallengeViewModel
@@ -31,42 +34,44 @@ class ScoreScreen : Fragment(), OnBackPressedDispatcherOwner {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-
-
         _binding = ScoreScreenBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val factory= InjectorUtils.provideChallengeViewModelFactory()
-        challengeViewModel =
-            //this returns challengeViewModel which manipulates challengeRepository
-            ViewModelProvider(this, factory).get(ChallengeViewModel::class.java)
-        challengeViewModel.getScore().observe(viewLifecycleOwner, Observer { user->
-            val stringBuilder=StringBuilder()
-            user.forEach { score ->
-                stringBuilder.append("${score.user} ${score.points}\n")
-            }
-            binding.scoreTextView.text=stringBuilder.toString()
-        })
+
         return root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val scoreItemList = mutableListOf<ScoreItem>()
+        val factory= InjectorUtils.provideChallengeViewModelFactory()
+        challengeViewModel =
+                //this returns challengeViewModel which manipulates challengeRepository
+            ViewModelProvider(this, factory).get(ChallengeViewModel::class.java)
+        challengeViewModel.getScore().observe(viewLifecycleOwner, Observer { user->
+            val stringBuilder=StringBuilder()
+            user.forEach { score ->
+                scoreItemList.add(ScoreItem(score.user, score.points))
+            }
+            recyclerView = view.findViewById(R.id.listRecycler)
+            val adapter = ScoreAdapter(scoreItemList.toList(), layoutInflater)
+            recyclerView.layoutManager= LinearLayoutManager(requireContext())
+            recyclerView.adapter = adapter
+        })
 
         val toolbar = (activity as MainActivity).findViewById<Toolbar>(R.id.toolbar)
-
         // Set a click listener for the back button in the app bar
         toolbar.setNavigationOnClickListener {
             // Navigate to the first fragment
             val navController = findNavController()
-            navController.popBackStack(R.id.welcomeScreen, false)
+            navController.popBackStack(R.id.welcomeScreen,  false)
         }
+
 
         onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             // Navigate to the first fragment
             val navController = findNavController()
-            navController.popBackStack(R.id.welcomeScreen, false)
+            navController.popBackStack(R.id.welcomeScreen,  false)
 
         }
     }
